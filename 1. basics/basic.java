5048d90c41d7c037b289aa6c0c278e337f26a0d0
RAMBO-LC <rambo4code@gmail.com>
2026-05-07 18:23:31 +0530

    commit

--- 1. basics/basic.java
+++ 1. basics/basic.java
@@ -1,3 +1,19 @@
 public class basic {
+    public static void main(Strings[] args){
+
+        //Variable => a reusable container fo a value
+        //            a variable behaves as if it  was the value it contains
+
+        // primitive var => simple values that are store in memory (stack) 
+        // reference var => memory adress (stack) that points to another location (heap)
+
+        //primitive vs reference
+        // int         String          
+        // double      array           
+        // boolean     Object
+        // char
+
+ 
+    }
     
 }
