456e1bf089ff7c3cd2c421c068dae1a6fca2456e
RAMBO-LC <rambo4code@gmail.com>
2026-05-08 09:23:35 +0530

    boolean

---
+++ user_input/boolean/inBool.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class inBool {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Are you a student? (true/false)");
+
+        boolean isStu = sc.nextBoolean();
+
+        if (isStu == true) {
+            System.out.print("you a student");
+        } else {
+            System.out.print("you not a student");
+        }
+
+        sc.close();
+
+    }
+
+}
