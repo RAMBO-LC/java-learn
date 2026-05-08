850d3f6611479e064ef38c4e765a094ed63516e6
RAMBO-LC <rambo4code@gmail.com>
2026-05-08 09:05:53 +0530

    Int input

--- user_input/Int/inInt.java
+++ user_input/Int/inInt.java
@@ -4,6 +4,10 @@ public class inInt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("whats your age?");
+        int age = sc.nextInt();
+
+        System.out.println(" your age : " + age);
         sc.close();
     }
 
