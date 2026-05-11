936c51c38734345dec39d6e738156ef091948609
RAMBO-LC <rambo4code@gmail.com>
2026-05-11 19:40:20 +0530

    Method calling

---
+++ 18.Methods/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        hbd();
+        hbd();
+        hbd();
+        hbd();
+        hbd();
+        hbd();
+        hbd();
+        hbd();
+    }
+    static void hbd(){
+        System.out.println("Happy Birthday");
+        System.out.println("Very Happy Birthday");
+        System.out.println(" Birthday dear");
+        System.out.println("so many wishes for for hbd\n");
+    }
+}
+
+//Method => a block of reusable code
+//    that is executed when called ().
+// D . R . Y Principle 
+// Don't Repeat Yourself 
---
+++ 18.Methods/readme.md
@@ -0,0 +1,3 @@
+Method => a block of reusable code
+
+that is executed when called ()
